1bbec18e0fcb017d9d1f7d3e1e045aece9d85b8a
Anton Rybochkin <anton.rybochkin@axibase.com>
2020-11-23 17:04:28 +0300

    Use ~/.npm-global as global modules directory

--- linkcheck.sh
+++ linkcheck.sh
@@ -12,7 +12,7 @@ if [[ "$ENABLE_CHECK" = "true" && -n "$(list_modified_md_files)" ]]; then
     if [ ! -f ".linkcheck-config.json" ]; then
         wget https://raw.githubusercontent.com/axibase/atsd/master/.linkcheck-config.json
     fi
-    list_modified_md_files | xargs -d '\n' -n1 markdown-link-check -v -c .linkcheck-config.json
+    list_modified_md_files | xargs -d '\n' -n1 ~/.npm-global/bin/markdown-link-check -v -c .linkcheck-config.json
 else
     echo "Link checking will be skipped"
 fi
